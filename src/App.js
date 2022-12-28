5850f2b5077f1a7638f1e0bf7a5c11d455ff0c44
jake <jakebrown536@gmail.com>
2022-12-27 19:04:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import MoleContainer from './MoleContainer'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  let [score, setScore] = useState(0)
 
-export default App;
+  const createMoleHill = () => {
+    let hills = []
+    for (let i = 0; i < 9; i++) {
+      hills.push(
+        <MoleContainer key={i} setScore={setScore} score={score} />
+      )
+    }
+
+    return (
+      <div style={{ backgroundColor: 'grey' }}>
+        {hills}
+      </div>
+    )
+  }
+  
+    return (
+      <div className="App">
+        <h1>React-a-Mole!</h1>
+        <p>{score}</p>
+        {createMoleHill()}
+      </div>
+    )
+  }
+
+
+export default App
